8f6354d9d85f65d62c0158f6f482fe81e69ce8e1
Zachary Danger Campbell <zacharydanger@fastmail.com>
2024-07-08 15:01:45 -0500

    Actually send messages

--- app/jobs/send_welcome_sms_job.rb
+++ app/jobs/send_welcome_sms_job.rb
@@ -1,7 +1,18 @@
 class SendWelcomeSmsJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(phone_number:)
     # Do something later
+    client.messages.create(
+      body: "Hey there, it's #{Time.now}. Seems like a clock would've been easier.",
+      to: phone_number,
+      from: ENV.fetch("TWILIO_PHONE_NO")
+    )
+  end
+
+  private
+
+  def client
+    @client ||= Twilio::REST::Client.new ENV.fetch("TWILIO_SID"), ENV.fetch("TWILIO_TOKEN")
   end
 end
