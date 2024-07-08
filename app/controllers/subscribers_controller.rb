62656d48e837f4d1ba7dc4d8a1560cd69a61be70
Zachary Danger Campbell <zacharydanger@fastmail.com>
2024-07-08 14:40:05 -0500

    stand up the create action

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -4,5 +4,24 @@ class SubscribersController < ApplicationController
   end
 
   def create
+    @subscriber = Subscriber.new(subscriber_params)
+
+    if @subscriber.save
+      respond_to do |format|
+        format.html { redirect_to action: :index, notice: "Subscriber created!" }
+        format.turbo_stream { flash.now[:notice] = "Subscriber created!" }
+      end
+    else
+      respond_to do |format|
+        format.html { render action: :new, status: :unprocessable_entity }
+        format.turbo_stream { flash.now[:error] = "Could not create subscriber." }
+      end
+    end
+  end
+
+  private
+
+  def subscriber_params
+    params.require(:subscriber).permit(:phone_number)
   end
 end
